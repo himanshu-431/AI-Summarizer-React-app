19dcddd23ed7f455bb426715b19fb2d30f1f2e06
himanshu-431 <himanshuchaurasia431@gmail.com>
2023-10-11 11:23:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Demo from './components/Demo';
+import Hero from './components/Hero';
+
+const App = () => {
+  return (
+    <main>
+      <div className='main'>
+         <div className='gradient' />
+      </div>
+
+      <div className='app'>
+         <Hero/>
+         <Demo/>
+      </div>
+    </main>
+    
+  )
+}
+
+export default App;
